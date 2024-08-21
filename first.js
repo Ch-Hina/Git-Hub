2657e9fafeb89f33eebc18da8fda8652c2180f73
Ch-Hina <chhinamustafa07@gmail.com>
2024-08-21 03:40:32 -0700

    Created table

---
+++ first.js
@@ -0,0 +1,15 @@
+
+
+
+var arr = [
+    {name: "Hina",     age: 19 , color:"pink" },
+    {name: "Amna" ,    age: 21 , color:"Blue" },
+    {name: "Ch Amna" , age: 19 , color:"pink" },
+    {name: "Aima " ,   age: 19 , color:"Black" }
+]
+
+
+
+for( var index =0; index <=3; index++){
+    console.log(arr[index])
+}
