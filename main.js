efee927b79b899d7cb3a0d21095af3fef4d0b3bf
Ch-Hina <chhinamustafa07@gmail.com>
2024-08-21 03:21:25 -0700

    basic table creation

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+var arr = [
+    {name: "Hina",     age: 19 , color:"pink" },
+    {name: "Amna" ,    age: 21 , color:"Blue" },
+    {name: "Ch Amna" , age: 19 , color:"pink" },
+    {name: "Aima " , age: 19 , color:"Black" }
+]
+
+
+
+var h1 =document.getElementsByTagName("h1")
+for(var i=0; i<= h1.length; i++){
+    console.log(h1[i].innerHTML);
+}
+
+
+
+
+var tbody = document.getElementsByTagName("tbody")
+console.log(tbody)
+
+
+
+for( var index= 0; index <= 3; index++){
+    var row = <tr>
+        <td> ${arr[index]} </td>
+        <td> ${arr[index]} </td>
+        <td> ${arr[index]} </td>
+
+    </tr>;
+} 
+    
